01f15dad545fd748fd7832c3b5e4704e6293f9bd
DavedCN <davednnadozie@gmail.com>
2024-01-04 23:39:10 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { HomePage } from "./component/homepage";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
